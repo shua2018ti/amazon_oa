06ac3b5d7c0f5fab6d486141937b9f8480fbe1e6
Tony Jiang <yanjunjiangjn@gmail.com>
2018-07-22 20:17:03 -0400

    Fix days change bugs (really interface in the Demo)

--- amazon/daysChange.cpp
+++ amazon/daysChange.cpp
@@ -1,38 +1,24 @@
-#include<vector>
-#include<iostream>
-using namespace std;
-
-class DaysChange {
-    public:
-    vector<int> Solution(vector<int>& days, int n) {
-        if (days.empty() || n <= 0)
-            return days;
-        int size = days.size();
-        vector<int> rvalue(size + 2);
-        rvalue[0] = rvalue[size + 1] = 0;
-        int pre = rvalue[0];
-        for (int i = 1; i <= size; i++)
-            rvalue[i] = days[i - 1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= size; j++) {
-                int temp = rvalue[j];
-                rvalue[j] = pre ^ rvalue[j + 1];
-                pre = temp;
-            }
+// INCLUDE HEADER FILES NEEDED BY YOUR PROGRAM
+// SOME LIBRARY FUNCTIONALITY MAY BE RESTRICTED
+// DEFINE ANY FUNCTION NEEDED
+// FUNCTION SIGNATURE BEGINS, THIS FUNCTION IS REQUIRED
+vector<int> cellCompete(int* states, int days) 
+{
+    const int SIZE = 8;
+    vector<int> ans(SIZE + 2);
+    ans[0] = ans[SIZE + 1] = 0;
+    for (int i = 1; i <= SIZE; i++)
+        ans[i] = states[i - 1];
+        
+    for (int i = 0; i < days; i++) {
+        int pre = ans[0]; // must reset pre each loop
+        for (int j = 1; j <= SIZE; j++) {
+            int tmp = ans[j];
+            ans[j] = pre ^ ans[j + 1];
+            pre = tmp;
         }
-        return vector<int>(rvalue.begin() + 1, rvalue.end() - 1);
     }
-};
-
-
-int main(void) {
-    DaysChange  dc;
-    vector<int> cells = {1, 0, 0, 0, 0, 1, 0, 0};
-    int days = 1;
-    vector<int> res = dc.Solution(cells, days);
-    for (int i = 0; i < res.size(); i++)
-        cout << res[i] << (i == res.size() - 1 ? "" : ",");
-    cout << endl;
-
-    return 0;
+    
+    return vector<int>(ans.begin() + 1, ans.end() - 1);
 }
+
